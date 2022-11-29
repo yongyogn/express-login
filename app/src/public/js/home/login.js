dcfc3cf67fb71f40ed702bdc7abd27fd648d1945
yongyogn <dotory1998@naver.com>
2022-11-29 09:49:20 +0900

    로그인 버튼 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,15 @@
 "use strict";
 
-// login.js와 login.ejs 파일 연결
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+      pw = document.querySelector("#pw"),
+      loginBtn = document.querySelector("#loginBtn");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+    console.log(req);
+}
