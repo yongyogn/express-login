f22d47b18b6f6c285a7bb55dce80ac8ae89e7a1e
yongyogn <dotory1998@naver.com>
2022-11-30 09:18:51 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,13 @@ function login() {
         id: id.value,
         pw: pw.value,
     };
-    console.log(req);
+
+    fetch("/login", {   // login 경로에서
+        method: "POST", // POST라는 메소드로
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req) // JSON 객체는 문자열로 감싸져서 출력됨
+                                  // 그냥 req는 {id: "id", pw: "pw"}형태, JSON.stringify(req)는 {"id": "id", "pw": "pw"} 형태
+    })
 }
